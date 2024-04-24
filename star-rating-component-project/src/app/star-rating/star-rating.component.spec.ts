83a8f15fcab4a19196b8c1b035082b17d1fd4a11
Søren Oest Balmer <oestbalmer@gmail.com>
2024-04-24 23:30:05 +0200

    unit tests

--- star-rating-component-project/src/app/star-rating/star-rating.component.spec.ts
+++ star-rating-component-project/src/app/star-rating/star-rating.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { StarRatingComponent } from './star-rating.component';
 
@@ -20,4 +21,17 @@ describe('StarRatingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 5 stars as default', () => {
+    const stars = fixture.debugElement.queryAll(By.css('.star'));
+    expect(stars.length).toBe(5);
+  })
+
+  
+  it('should have 7 stars if changed to it', () => {
+    component.maxRating = 7;
+    fixture.detectChanges();
+    const stars = fixture.debugElement.queryAll(By.css('.star'));
+    expect(stars.length).toBe(7);
+  })
 });
